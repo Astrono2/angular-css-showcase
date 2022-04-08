a4bc691a44b53c98375ba462e469b43e3b9d1c4b
Astrono2 <jerescharovsky@gmail.com>
2022-04-08 19:00:08 -0300

    Added info button

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
 	selector: 'app-toolbar',
@@ -9,8 +9,14 @@ export class ToolbarComponent implements OnInit {
 	@Input() isThemeDark: boolean = false;
 	@Output('toggleTheme') toggleTheme: EventEmitter<any> = new EventEmitter();
 
+	@ViewChild('info') info!: ElementRef<any>;
+
 	isIconCurrent(isIconDark: boolean): string {
 		return isIconDark == this.isThemeDark ? 'current' : '';
+	}
+
+	showInfo() {
+		this.info.nativeElement.classList.toggle('visible');
 	}
 
 	constructor() { }
